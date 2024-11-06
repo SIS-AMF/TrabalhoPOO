8e5bd45ea5cc5861cc04c2e48f3b09abd22249de
LucasGFeldmann <lucasgfeldmann@gmail.com>
2024-11-06 10:47:02 -0300

    fix: adicionado id para cidades para ter um indentificador unico.

--- Cidade.java
+++ Cidade.java
@@ -1,13 +1,23 @@
 
 public class Cidade {
-
+    private final int id;
     private final String nome;
 
-    public Cidade(String nome) {
+    public Cidade(int id,String nome) {
         this.nome = nome;
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void exibir() {
+        System.out.println("ID: " + id);
+        System.out.println("Nome: " + nome);
     }
 }
